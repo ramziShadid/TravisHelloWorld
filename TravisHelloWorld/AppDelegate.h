13434aca1eb401ffef298c3f2ec995ebb34928dc
ramziShadid <ramzi_shadid@hotmail.com>
2017-07-12 11:22:35 +0300

    First Commit

---
+++ TravisHelloWorld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TravisHelloWorld
+//
+//  Created by Ramzi Shadid on 7/12/17.
+//  Copyright © 2017 Ramzi Shadid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TravisHelloWorld/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TravisHelloWorld
+//
+//  Created by Ramzi Shadid on 7/12/17.
+//  Copyright © 2017 Ramzi Shadid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
